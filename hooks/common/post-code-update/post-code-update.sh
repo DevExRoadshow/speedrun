c076e1d8b412e72d936112964722121d1245b245
balsama <balsam.adam@gmail.com>
2017-05-04 14:52:46 -0400

    debug

--- hooks/common/post-code-update/post-code-update.sh
+++ hooks/common/post-code-update/post-code-update.sh
@@ -20,7 +20,9 @@ deployed_tag="$4"
 repo_url="$5"
 repo_type="$6"
 
-drush @$site.$target_env status | grep -q 'Successful' && echo -e 'Site already installed. Skipping drush site-install' || drush @$site.$target_env site-install lightning --yes --account-pass=admin && drush @$site.$target_env pm-enable --yes config_split && drush @$site.$target_env config-set --yes "system.site" uuid "426134d7-3f71-415d-ab23-454ddcfdc6b7"
+echo 'FOOOOOO. Target env: ' . $target_env;
 
-  . /var/www/html/$site.$target_env/vendor/acquia/blt/scripts/cloud-hooks/functions.sh
-  deploy_updates
+#drush @$site.$target_env status | grep -q 'Successful' && echo -e 'Site already installed. Skipping drush site-install' || drush @$site.$target_env site-install lightning --yes --account-pass=admin && drush @$site.$target_env pm-enable --yes config_split && drush @$site.$target_env config-set --yes "system.site" uuid "426134d7-3f71-415d-ab23-454ddcfdc6b7"
+
+#. /var/www/html/$site.$target_env/vendor/acquia/blt/scripts/cloud-hooks/functions.sh
+#deploy_updates
