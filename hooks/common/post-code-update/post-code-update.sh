cfb1f7e4ef4cff9a3a4abb9093026a930355d5e8
Adam Balsam <balsama@users.noreply.github.com>
2017-05-05 09:20:27 -0400

    Use production DB when running tests in pipelines (#6)

--- hooks/common/post-code-update/post-code-update.sh
+++ hooks/common/post-code-update/post-code-update.sh
@@ -20,7 +20,20 @@ deployed_tag="$4"
 repo_url="$5"
 repo_type="$6"
 
-drush @$site.$target_env status | grep -q 'Successful' && echo -e 'Site already installed. Skipping drush site-install' || drush @$site.$target_env site-install lightning --yes --account-pass=admin && drush @$site.$target_env pm-enable --yes config_split && drush @$site.$target_env config-set --yes "system.site" uuid "426134d7-3f71-415d-ab23-454ddcfdc6b7"
+re="^ode*"
+if [[ $target_env =~ $re ]]
+then
+  echo 'Target name is: ' $target_env '. This is a CDE.';
+
+  # Fresh install of Lightning if this is a new environment.
+  drush @$site.$target_env status | grep -q 'Successful' && echo -e 'Site already installed. Skipping drush site-install' || drush @$site.$target_env site-install lightning --yes --account-pass=admin;
+
+  # Since we can't copy databases into CDEs yet, manually set the site UUID so
+  # that we can at least run a config import.
+  drush @$site.$target_env config-set --yes "system.site" uuid "426134d7-3f71-415d-ab23-454ddcfdc6b7"
+else
+  echo 'Target name is: ' $target_env '. Not a CDE';
+fi
 
   . /var/www/html/$site.$target_env/vendor/acquia/blt/scripts/cloud-hooks/functions.sh
   deploy_updates
